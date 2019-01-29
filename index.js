f0959626ffa85aaa15b9f06a27e41b1b76d65436
justin <justin@gomaji.com>
2019-01-29 18:47:16 +0800

    update drone.yml

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get('/', function (req, res) {
   res.send('Hello World! ' + myip);
 });
 
-var server = app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var server = app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
 
 module.exports = server
